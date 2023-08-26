ae8ca77f845af1e2403743095afae29cf7cbeaed
GauravRajpt <joygrajput@gmail.com>
2023-08-26 17:36:33 +0530

    complete maiin poster

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import rocket from '../helper/rocket.png'
+import mobile from '../helper/mobile.png'
 
 function Main() {
   return (
-    <div>Main</div>
+    <section>
+
+    <div className='main-poster'>
+    <div className='headings'>
+    <img src={rocket} alt='rocket'/>
+    <h1>Grow Faster Together</h1>
+    <h2>Find a mentor or Lead the path for someone<br/>
+    It’s a win win!</h2>
+    <div className='poster-btn'>
+    <h3>Find a Mentor</h3>
+    <h3>Become a Mentor</h3>
+    </div>
+    </div>
+    <div className='mobileImg'>
+    <img src={mobile} alt='mobile'/>
+    </div>
+    </div>
+    </section>
   )
 }
 
