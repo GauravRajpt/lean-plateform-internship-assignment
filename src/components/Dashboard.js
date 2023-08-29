2def4c0ff39b56ae958a11ec5322f5fa547fc264
GauravRajpt <joygrajput@gmail.com>
2023-08-29 09:00:46 +0530

    working on dashbord nav

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { remove } from '../store/navslice'
+import user from '../helper/user.png'
 
 function Dashboard() {
     const dispatch= useDispatch()
@@ -8,7 +9,32 @@ function Dashboard() {
         dispatch(remove());
     },[])
   return (
-    <div>Dashboard</div>
+    <div>
+      <div className='sidenav'>
+      <div className='user'>
+      <img src={user} alt='user'/>
+      <h2>Neha Bhat</h2>
+      </div>
+      <ul className='menu'>
+      <li>
+      <div className='svg'>
+      <svg xmlns="http://www.w3.org/2000/svg" width="23" height="19" viewBox="0 0 23 19" fill="none">
+  <path d="M8.94118 19V12.2941H13.4118V19H19V10.0588H22.3529L11.1765 0L0 10.0588H3.35294V19H8.94118Z" fill="black"/>
+</svg>
+</div>
+ <h2>Home</h2></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      </ul>
+      
+      </div>
+    </div>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,43 @@
-import React from 'react'
-import Logo from '../helper/Logo.png'
-import vector from '../helper/vector.png'
-import '../App.css'
-import { useSelector } from 'react-redux'
+import React from "react";
+import Logo from "../helper/Logo.png";
+import vector from "../helper/vector.png";
+import "../App.css";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 function Header() {
-
-  const nav= useSelector((state)=>state.nav)
+  const nav = useSelector((state) => state.nav);
   return (
     <nav>
-    <img className='logo' src={Logo} alt="logo"/>
-    <h2>Filters <img src={vector} alt="vector"/></h2>
-    <h2>About Us</h2>
-    <h2>Blog</h2>
-    {nav?
-      <><h3 className='Earning-calc'>Earning calculator</h3>
-    <div className='login-btns'>
-        <h3 className='login'>Login</h3>
-        <h3 className='signup'>Signup</h3></div></>:
-
-        <><h3 className='Earning-calc' style={{visibility:'hidden'}}>Earning calculator</h3>
-        <div className='login-btns' style={{visibility:'hidden'}}>
-            <h3 className='login'>Login</h3>
-            <h3 className='signup'>Signup</h3></div></>}
-    
+      <img className="logo" src={Logo} alt="logo" />
+      <h2>
+        Filters <img src={vector} alt="vector" />
+      </h2>
+      <h2>About Us</h2>
+      <h2>Blog</h2>
+      {nav ? (
+        <>
+          <h3 className="Earning-calc">Earning calculator</h3>
+          <div className="login-btns">
+            <h3 className="login">
+              <Link to="/dashboard" className="Link">Login</Link>
+            </h3>
+            <h3 className="signup">Signup</h3>
+          </div>
+        </>
+      ) : (
+        <>
+          <h3 className="Earning-calc" style={{ visibility: "hidden" }}>
+            Earning calculator
+          </h3>
+          <div className="login-btns" style={{ visibility: "hidden" }}>
+            <h3 className="login">Login</h3>
+            <h3 className="signup">Signup</h3>
+          </div>
+        </>
+      )}
     </nav>
-  )
+  );
 }
 
-export default Header
+export default Header;
